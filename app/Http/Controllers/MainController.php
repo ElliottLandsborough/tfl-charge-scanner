a4456e6c8e59b0ea100bf191f079690c3e8cb0b3
Elliott Landsborough <elliott.landsborough@gmail.com>
2019-01-08 13:45:12 +0000

    Get tfl monthly spend

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use GuzzleHttp\Client;
 
 class MainController extends Controller
 {
@@ -31,7 +32,80 @@ class MainController extends Controller
                 die('Incorrect State');
             }
 
-            echo $code;
+            $form_params = [
+                'grant_type' => 'authorization_code',
+                'client_id' => env('MONZO_CLIENT_ID'),
+                'client_secret' => env('MONZO_CLIENT_SECRET'),
+                'redirect_uri' => env('MONZO_REDIRECT_URI'),
+                'code' => $code
+            ];
+
+            $response = (new client())->request('POST', 'https://api.monzo.com/oauth2/token', [
+                'form_params' => $form_params
+            ]);
+
+            $body = $response->getBody();
+
+            $stringBody = (string) $body;
+
+            $auth = json_decode($stringBody);
+
+            $auth->time = time();
+
+            $request->session()->put('monzo_auth', $auth);
+        }
+
+        // auth exists, try to do an api request
+        if ($request->session()->has('monzo_auth')) {
+            $monzo_auth = $request->session()->get('monzo_auth');
+
+            $response = (new client())->request('GET', 'https://api.monzo.com/accounts', [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $monzo_auth->access_token
+                ]
+            ]);
+
+            $body = $response->getBody();
+
+            $stringBody = (string) $body;
+
+            $response = json_decode($stringBody);
+
+            foreach ($response->accounts as $account) {
+                if ($account->type == 'uk_retail') {
+                    $account_id = $account->id;
+                }
+            }
+
+            $response = (new client())->request('GET', "https://api.monzo.com/transactions?account_id=$account_id", [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $monzo_auth->access_token
+                ]
+            ]);
+
+            $body = $response->getBody();
+
+            $stringBody = (string) $body;
+
+            $response = json_decode($stringBody);
+
+            $transactions = [];
+
+            foreach ($response->transactions as $transaction) {
+                // detect tfl
+                if (isset($transaction->metadata->notes) && strpos(strtolower($transaction->metadata->notes), 'travel charge for') !== false) {
+
+                    $year = substr($transaction->created, 0, 4);
+                    $month = substr($transaction->created, 5, 2);
+
+                    if (!isset($transactions[$year])) $transactions[$year] = [];
+                    if (!isset($transactions[$year][$month])) $transactions[$year][$month] = 0;
+
+                    $transactions[$year][$month] += $transaction->amount / 100;
+                }
+            }
+
+            print_r($transactions);
 
             die;
         }
