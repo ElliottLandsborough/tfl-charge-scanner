44a4a2ed0735bbd5cd77b8f0c4fc7f8587ab4055
Elliott Landsborough <elliott.landsborough@gmail.com>
2019-01-11 10:42:05 +0000

    Remove author tag

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta name="description" content="">
-<meta name="author" content="Scotch">
+<meta name="author" content="">
 <title>Monzo TFL Charges</title>
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <meta name="csrf-token" content="{{ csrf_token() }}">
