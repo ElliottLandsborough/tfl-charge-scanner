c147e0eb9f3eeca97e817d9fe3d86a01e751c2eb
Elliott Landsborough <elliott.landsborough@gmail.com>
2019-01-23 12:01:48 +0000

    Comment all the js

--- resources/js/components/Home.js
+++ resources/js/components/Home.js
@@ -9,38 +9,54 @@ import Loader from './Loader'
 
 class Example extends Component {
 
-  initialState() {
-    return {
-      monzoApi: 'https://api.monzo.com',
-      error: null,
-      isAuthorized: false,
-      accessToken: false,
-      accounts: [],
-      accountId: false,
-      transactions: [],
-      transactionsForTravel: [],
-      travelTransactionsLastDate: false,
-      yearAverages: [],
-      yearTotals: [],
-      yearMonths: [],
-      fromZone: 0,
-      toZone: 0,
-      yearlyAmount: 0,
-      clubAmount: 0,
-      sinceDate: false,
-      loadingIsComplete: false,
-      fullTotal: 0,
-    };
-  }
-
+  /**
+   * Constructor
+   * @param  {[type]} props [description]
+   * @return {[type]}       [description]
+   */
   constructor(props) {
     super(props)
+
+    // set the initial state
     this.state = this.initialState();
+
+    // bind 'this' to a few functions
     this.setFromZone = this.setFromZone.bind(this);
     this.setToZone = this.setToZone.bind(this);
     this.logOut = this.logOut.bind(this);
   }
 
+  /**
+   * Returns an object of the default state for this component
+   * @return {object}
+   */
+  initialState() {
+    return {
+      monzoApi: 'https://api.monzo.com', // monzo api url
+      error: null, // in case an error happens
+      isAuthorized: false, // becomes true if an auth token exists
+      accessToken: false, // the access token returned from the api
+      accounts: [], // the accounts returned by the api
+      accountId: false, // the 'retail_uk' account id
+      transactionsForTravel: [], // transactions used for TFL travel
+      travelTransactionsLastDate: false, // the date of the most recent transaction
+      yearAverages: [], // yearly average monzo spend
+      yearTotals: [], // yearly total monzo spend
+      yearMonths: [], // monthly total monzo spend
+      fromZone: 0, // current 'from' zone from dropdowns
+      toZone: 0, // current 'to' zone from dropdowns
+      yearlyAmount: 0, // current yearly tfl price based on dropdowns
+      clubAmount: 0, // current commuterclub price based on dropdowns
+      sinceDate: false, // when to count transactions from
+      loadingIsComplete: false, // becomes true when transaction loop finishes
+      fullTotal: 0, // all monzo tfl transactions added together
+    };
+  }
+
+  /**
+   * returns the official TFL prices
+   * @return {array} zones, weekly, monthly, yearly
+   */
   getTflAmounts() {
     // http://content.tfl.gov.uk/adult-fares-2019.pdf
     const amounts = [
@@ -94,14 +110,21 @@ class Example extends Component {
     return amounts;
   }
 
+  /**
+   * Returns the price per year based on a start/finish zone
+   * @type {Number}
+   */
   getYearlyAmount(start = 1, finish = 3)
   {
-    if (start > finish) {
-      [start, finish] = [finish, start]; // swap the vars, es6
-    }
-
+    // init vars
     let amount = 0, array, amounts = this.getTflAmounts();
 
+    // if start is higher than finish, switch the vars around
+    if (start > finish) {
+      [start, finish] = [finish, start];
+    }
+
+    // search for match
     for (array of amounts) {
       if (array[0] == (start + '-' + finish)) {
         amount = array[3];
@@ -111,14 +134,34 @@ class Example extends Component {
     return amount;
   }
 
+  /**
+   * Calculate how much commuter club should cost based on a yearly price
+   * @type {Number}
+   */
   calculateCommuterClub(yearlyAmount)
   {
     // REPRESENTATIVE EXAMPLE: Credit Limit: £1200. Interest: £67 Total payable: £1267 in 11 monthly instalments of £115. Representative 11.61% APR. Interest rate: 5.6% pa
     return ((yearlyAmount / 100 * 5.6) + yearlyAmount);
   }
 
+  /**
+   * Runs once the component is initialized
+   * @return {[type]} [description]
+   */
   componentDidMount() {
+    // make sure the state contains the default amounts
     this.setAmounts();
+
+    // initialize auth, get an access token from laravel
+    this.initializeAuthentication();
+  }
+
+  /**
+   * Check if we already have an access token stored in the session.
+   * If yes, store in state for later use.
+   * @return {[type]} [description]
+   */
+  initializeAuthentication() {
     fetch("/credentials")
       .then(res => res.json())
       .then(
@@ -144,23 +187,23 @@ class Example extends Component {
       .then(
         (howManyItems) => {
           if (howManyItems) {
+            // If yes, query the api for an account id
             this.fetchAccountId();
           }
         }
       )
   }
 
+  /**
+   * Get the account id from the monzo api
+   * @return {[type]} [description]
+   */
   fetchAccountId() {
     const { monzoApi } = this.state;
     let self = this;
     fetch(monzoApi + '/accounts', this.authParams())
       .then(function(response) {
         if(response.status !== 200) {
-          /*
-          self.setState({
-            isAuthorized: false,
-            accessToken: false
-          });*/
           // did not get a 200, log the user out
           // TODO: some kind of error message?
           this.logOut();
@@ -170,6 +213,7 @@ class Example extends Component {
       .then(res => res.json())
       .then(
         (result) => {
+          // success, add accounts to state
           this.setState({
             accounts: result.accounts
           });
@@ -182,16 +226,22 @@ class Example extends Component {
       )
       .then(
           () => {
+            // success, try to extract retail account id
             this.setRetailAccountId();
           }
       )
       .then(
           () => {
+            // success, try to get transactions from api
             this.populateTransactions();
           }
       )
   }
 
+  /**
+   * Check api response for account that matches 'uk_retail'.
+   * Add to state if match exists.
+   */
   setRetailAccountId() {
     const { accounts } = this.state;
 
@@ -204,6 +254,11 @@ class Example extends Component {
     }.bind(this));
   }
 
+  /**
+   * Returns a url string generated from an object with keys
+   * @param  {object} params {var1: string, var2: lol}
+   * @return {string}        'var1=string&var2=lol'
+   */
   urlEncode(params) {
     var out = [];
 
@@ -216,6 +271,11 @@ class Example extends Component {
     return out.join('&');
   }
 
+  /**
+   * Returns the auth params
+   * @param  {String} method Http verb e.g GET/POST
+   * @return {Object}        The auth params
+   */
   authParams(method = 'GET') {
     const { accessToken } = this.state;
     return {
@@ -226,6 +286,9 @@ class Example extends Component {
     }
   }
 
+  /**
+   * Set the date to start counting transactions from
+   */
   setSinceDate() {
     const since = moment().subtract(12, 'months').format('Y-MM') + '-01T00:00:00Z';
     this.setState({sinceDate: since});
@@ -248,15 +311,28 @@ class Example extends Component {
     return monzoApi + '/transactions?' + this.urlEncode(params);
   }
 
+  /**
+   * Calculate how many days between when we started counting from
+   * and what date the api has returned (for the progress bar).
+   * @return {Number} Between 0 and 100 usually.
+   */
   getDaysPercentage()
   {
+    // this can only happen if we have both a start date and the api has returned some transactions
     if (this.state.sinceDate !== false && this.state.transactionsForTravel.length) {
-        const lastDateString = this.state.transactionsForTravel[this.state.transactionsForTravel.length-1].created;
-        const nowDate = moment();
+        // date we start querying api from
         const fromDate = moment(this.state.sinceDate);
+        // now
+        const nowDate = moment();
+        // last date from list of transactions (string)
+        const lastDateString = this.state.transactionsForTravel[this.state.transactionsForTravel.length-1].created;
+        // create date object from string
         const lastDate = moment(lastDateString);
+        // day count between now and when we started querying the api
         const fromStartUntilNow = moment.duration(nowDate.diff(fromDate)).as('days');
+        // day count between when we started querying the api and the last thing the api returned
         const fromstartUntilLast = moment.duration(lastDate.diff(fromDate)).as('days');
+        // calc percentage
         const percentage = Math.round(fromstartUntilLast / fromStartUntilNow * 100);
 
         return percentage;
@@ -265,16 +341,28 @@ class Example extends Component {
     return 0;
   }
 
+  /**
+   * Find out how much someone spends per month
+   * @return {Number} (e.g 92.47)
+   */
   getMonzoMonthlyAverage()
   {
+    // convert the full total to a positive number of pounds/pence
     const fullTotal = this.state.fullTotal * -1 / 100;
+    // only bother if a start date was set and the total is bigger than 0
     if (this.state.sinceDate !== false && fullTotal > 0) {
-        const daysInMonth = 30.44 // average number of days in a month including leaps
+        // average number of days in a month including leaps
+        const daysInMonth = 30.44;
+        // date object of start date
         const fromDate = moment(this.state.sinceDate);
+        // date object of now
         const nowDate = moment();
+        // how many days exist between start and now?
         const fromStartUntilNow = moment.duration(nowDate.diff(fromDate)).as('days');
+        // and this equates to how many months (e.g 12.76)
         const exactMonths = fromStartUntilNow / daysInMonth;
 
+        // the total equates to this much per month
         return (fullTotal / exactMonths).toFixed(2);
     }
 
@@ -283,6 +371,7 @@ class Example extends Component {
 
   // this is a bit weird because we have to call the api once at a time...
   // maybe do it by month instead?
+  // TODO: clean this up!!!
   populateTransactions() {
     this.setSinceDate();
 
@@ -349,35 +438,49 @@ class Example extends Component {
     transactionsLoop();
   }
 
+  /**
+   * Store some parts the state in localstorage
+   */
   storeTravelTransactions()
   {
     localStorage.setItem('travelTransactions', JSON.stringify(this.state.transactionsForTravel));
     localStorage.setItem('travelTransactionsLastDate', JSON.stringify(this.state.transactionsForTravel[this.state.transactionsForTravel.length-1].created));
   }
 
+  /**
+   * Calculate the totals and averages from the transactions in the state
+   * @return {[type]} [description]
+   */
   travelTotals() {
+    // some defaults
     let yearAverages = {};
     let yearTotals = {};
     let yearMonths = {};
     let fullTotal = 0;
 
+    // loop through all transactions
     this.state.transactionsForTravel.forEach(function(transaction) {
       const amount = transaction.amount;
       const month = transaction.created.substr(5,2);
       const year = transaction.created.substr(0,4);
 
+      // total per year, initialize as 0 if it doesnt exist yet
       if (typeof yearTotals[year] == "undefined") yearTotals[year] = 0;
       yearTotals[year] += amount;
 
+      // average per year, initialize as 0 if it doesnt exist yet
       if (typeof yearAverages[year] == "undefined") yearAverages[year] = 0;
       yearAverages[year] += Math.round(amount / 12);
 
+      // total per yearMonth initialize as 0 if it doesnt exist yet
       if (typeof yearMonths['' + year + month] == "undefined") yearMonths['' + year + month] = 0;
       yearMonths['' + year + month] += amount;
 
+      // total of everything
       fullTotal += amount;
     });
 
+    // add all calculations to the state
     this.setState({
         yearAverages: yearAverages,
         yearTotals: yearTotals,
@@ -386,34 +489,42 @@ class Example extends Component {
     });
   }
 
+  // runs whenever 'from' checkbox is changed
   setFromZone(event) {
     this.setAmounts(event.target.value, this.state.toZone);
   }
 
+  // runs whenever 'to' checkbox is changed
   setToZone(event) {
     this.setAmounts(this.state.fromZone, event.target.value);
   }
 
+  /**
+   * Set the to/from zones and the yearly tfl/commuterclub prices in the state
+   * @param {Number} fromZone [description]
+   * @param {Number} toZone   [description]
+   */
   setAmounts(fromZone = 1, toZone = 3) {
     const yearlyAmount = this.getYearlyAmount(fromZone, toZone);
     const clubAmount = this.calculateCommuterClub(yearlyAmount);
     this.setState({fromZone: fromZone, toZone: toZone, yearlyAmount: yearlyAmount, clubAmount: clubAmount});
   }
 
+  /**
+   * Logs a user out
+   * @return {[type]} [description]
+   */
   logOut() {
     // async get all logout urls, no need to process response
-    fetch(this.state.monzoApi + '/oauth2/logout', this.authParams('POST'));
-    fetch('/logout');
+    fetch(this.state.monzoApi + '/oauth2/logout', this.authParams('POST')); // log out of monzo api
+    fetch('/logout'); // log out of laravels everything
 
-    // reset state
-    this.setState({
-        isAuthorized: false,
-        accessToken: false
-    });
+    // reset state to initial values
+    this.setState(this.initialState());
 
-    // reset localstorage
-    localStorage.setItem('travelTransactions', JSON.stringify([]));
-    localStorage.setItem('travelTransactionsLastDate', JSON.stringify(false));
+    // clear localstorage
+    //localStorage.setItem('travelTransactions', JSON.stringify([]));
+    //localStorage.setItem('travelTransactionsLastDate', JSON.stringify(false));
     localStorage.clear();
   }
 
