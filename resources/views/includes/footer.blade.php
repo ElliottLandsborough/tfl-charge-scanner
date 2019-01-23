455a369575727307fd08dfdf4fbd1f48108331b7
Elliott Landsborough <elliott.landsborough@gmail.com>
2019-01-23 13:28:33 +0000

    Some affiliate links

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,6 @@
+<div class="affiliates">
+    <p>Visit the <a href="https://www.commuterclub.co.uk/dsajkdasjkl" target="_blank">Commuter Club website</a>. Have you considered buying <a href="https://amzn.to/2RK1qCz" target="_blank">a bike from amazon</a>?</p>
+</div>
 <footer class="footer">
 <p>No data is stored by this application</p>
 <p>Visit my <a href="https://github.com/ElliottLandsborough" target="_blank">GitHub profile</a></p>
