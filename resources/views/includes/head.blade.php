0dadaf1ac67d1d7eb5d25b39e474ca60ad7b1373
Elliott Landsborough <elliott.landsborough@gmail.com>
2019-01-24 11:14:19 +0000

    Add author info

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,6 +1,16 @@
 <meta charset="utf-8">
 <meta name="description" content="">
-<meta name="author" content="">
-<title>Monzo TFL Charges</title>
+
+<meta name="author" content="Elliott Landsborough">
+<link rel="author" href="humans.txt" />
+
+<title>Monzo TFL charge scanner</title>
+
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="description" content="Check your monzo account for london transport charges.">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<link rel="apple-touch-icon" href="icon.png">
+
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <meta name="csrf-token" content="{{ csrf_token() }}">
