5ec26b123cb2b5e0a95ceacfeab52e918c641390
Elliott Landsborough <elliott.landsborough@gmail.com>
2019-01-25 16:47:51 +0000

    Force https on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 
     /**
