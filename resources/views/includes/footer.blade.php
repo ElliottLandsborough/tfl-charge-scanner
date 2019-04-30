14aba100acce0cbdfccfa79433c53d576c5364c9
Elliott L <elliott.landsborough@gmail.com>
2019-04-30 09:23:45 +0100

    Update github link

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,5 +3,5 @@
 </div>
 <footer class="footer">
 <p>No data is stored by this application</p>
-<p>Visit my <a href="https://github.com/ElliottLandsborough" target="_blank">GitHub profile</a></p>
+<p>Visit my <a href="https://github.com/ElliottLandsborough/monzo-balance" target="_blank">GitHub profile</a></p>
 </footer>
