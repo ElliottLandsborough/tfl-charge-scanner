69929cded80559aed3ff8af84284048ca5a55481
Elliott L <elliott.landsborough@gmail.com>
2019-04-30 09:36:24 +0100

    Fix link

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="affiliates">
-    <p>Visit the <a href="https://www.commuterclub.co.uk/dsajkdasjkl" target="_blank">Commuter Club website</a>. Have you considered buying a <a href="https://amzn.to/2RK1qCz" target="_blank">bicycle from Amazon</a>?</p>
+    <p>Visit the <a href="https://www.commuterclub.co.uk/" target="_blank">Commuter Club website</a>. Have you considered buying a <a href="https://amzn.to/2RK1qCz" target="_blank">bicycle from Amazon</a>?</p>
 </div>
 <footer class="footer">
 <p>No data is stored by this application.</p>
