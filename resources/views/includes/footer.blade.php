aad4dc0df2be58402ef267415095ab2e925a39fe
Elliott L <elliott.landsborough@gmail.com>
2019-04-30 09:32:31 +0100

    Forgot full stop.

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,6 +2,6 @@
     <p>Visit the <a href="https://www.commuterclub.co.uk/dsajkdasjkl" target="_blank">Commuter Club website</a>. Have you considered buying a <a href="https://amzn.to/2RK1qCz" target="_blank">bicycle from Amazon</a>?</p>
 </div>
 <footer class="footer">
-<p>No data is stored by this application</p>
+<p>No data is stored by this application.</p>
 <p>Visit my <a href="https://github.com/ElliottLandsborough/monzo-balance" target="_blank">GitHub profile</a></p>
 </footer>
