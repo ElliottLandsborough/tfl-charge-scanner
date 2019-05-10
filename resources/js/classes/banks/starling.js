f8ad491c413d4b0cf23053869d58d3d594eb1b51
Elliott L <elliott.landsborough@gmail.com>
2019-05-10 17:10:28 +0100

    Update starling object with notes

--- resources/js/classes/banks/starling.js
+++ resources/js/classes/banks/starling.js
@@ -1,9 +1,19 @@
 import Bank from '../bank'
 
 class Starling extends Bank {
-    whatami() {
-        console.log('starling');
-    }
+  apiUrl = 'https://api-sandbox.starlingbank.com';
+  // apiUrl = 'https://api.starlingbank.com';
+
+  /**
+   * Get the account id from the monzo api
+   * @return {[type]} [description]
+   */
+  fetchAccountId(accessToken) {
+    // todo - php piper
+  }
+
+  logout(accessToken = false) {
+  }
 }
 
-export default Starling;
+export default Starling1;
